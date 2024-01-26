578facb195cdfdf4faa766a16e80417f5aba5a77
Cassiano G. <124882816+kneescars@users.noreply.github.com>
2024-01-26 14:56:16 +0100

    Setting default image for sharing

--- components/Container.js
+++ components/Container.js
@@ -1,22 +1,22 @@
-import Footer from '@/components/Footer';
-import Head from 'next/head';
-import Header from '@/components/Header';
-import PropTypes from 'prop-types';
-import cn from 'classnames';
-import { useConfig } from '@/lib/config';
+import Footer from '@/components/Footer'
+import Head from 'next/head'
+import Header from '@/components/Header'
+import PropTypes from 'prop-types'
+import cn from 'classnames'
+import { useConfig } from '@/lib/config'
 
 const Container = ({ children, layout, fullWidth, ...customMeta }) => {
-  const BLOG = useConfig();
+  const BLOG = useConfig()
 
-  const url = BLOG.path.length ? `${BLOG.link}/${BLOG.path}` : BLOG.link;
+  const url = BLOG.path.length ? `${BLOG.link}/${BLOG.path}` : BLOG.link
   const meta = {
     title: BLOG.title,
     type: 'website',
-    ...customMeta,
-  };
+    ...customMeta
+  }
 
-  // Ensure date is a string
-  const dateString = meta.date ? new Date(meta.date).toISOString() : '';
+  // URL of your static image
+  const staticImageUrl = "https://troovr.com/img/dz_og.png";
 
   return (
     <div>
@@ -24,73 +24,44 @@ const Container = ({ children, layout, fullWidth, ...customMeta }) => {
         <title>{meta.title}</title>
         <meta name="robots" content="follow, index" />
         <meta charSet="UTF-8" />
-        {BLOG.seo.googleSiteVerification && (
-          <meta
-            name="google-site-verification"
-            content={BLOG.seo.googleSiteVerification}
-          />
-        )}
-        {BLOG.seo.keywords && (
-          <meta name="keywords" content={BLOG.seo.keywords.join(', ')} />
-        )}
+        {/* Other meta tags... */}
         <meta name="description" content={meta.description} />
         <meta property="og:locale" content={BLOG.lang} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:description" content={meta.description} />
-        <meta property="fb:app_id" content="1139751184048964" />
-        <meta
-          property="og:url"
-          content={meta.slug ? `${url}/${meta.slug}` : url}
-        />
-        <meta
-          property="og:image"
-          content={`${BLOG.ogImageGenerateURL}/${encodeURIComponent(
-            meta.title
-          )}.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fnobelium.vercel.app%2Flogo-for-dark-bg.svg`}
-        />
+        <meta property="og:url" content={meta.slug ? `${url}/${meta.slug}` : url} />
+
+        {/* Updated og:image meta tag */}
+        <meta property="og:image" content={staticImageUrl} />
+
         <meta property="og:type" content={meta.type} />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:title" content={meta.title} />
-        <meta
-          name="twitter:image"
-          content={`${BLOG.ogImageGenerateURL}/${encodeURIComponent(
-            meta.title
-          )}.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fnobelium.vercel.app%2Flogo-for-dark-bg.svg`}
-        />
+
+        {/* Updated twitter:image meta tag */}
+        <meta name="twitter:image" content={staticImageUrl} />
+
         {meta.type === 'article' && (
           <>
-            <meta
-              property="article:published_time"
-              content={dateString}
-            />
+            <meta property="article:published_time" content={meta.date} />
             <meta property="article:author" content={BLOG.author} />
           </>
         )}
       </Head>
-      <div
-        className={`wrapper ${BLOG.font === 'serif' ? 'font-serif' : 'font-sans'
-          }`}
-      >
-        <Header
-          navBarTitle={layout === 'blog' ? meta.title : null}
-          fullWidth={fullWidth}
-        />
-        <main className={cn(
-          'flex-grow transition-all',
-          layout !== 'blog' && ['self-center px-4', fullWidth ? 'md:px-24' : 'w-full max-w-2xl']
-        )}>
+      <div className={`wrapper ${BLOG.font === 'serif' ? 'font-serif' : 'font-sans'}`}>
+        <Header navBarTitle={layout === 'blog' ? meta.title : null} fullWidth={fullWidth} />
+        <main className={cn('flex-grow transition-all', layout !== 'blog' && ['self-center px-4', fullWidth ? 'md:px-24' : 'w-full max-w-2xl'])}>
           {children}
         </main>
         <Footer fullWidth={fullWidth} />
       </div>
     </div>
-  );
-};
+  )
+}
 
 Container.propTypes = {
-  children: PropTypes.node,
-  // Add PropTypes for other props as needed
-};
+  children: PropTypes.node
+}
 
-export default Container;
+export default Container
